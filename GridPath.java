23dad8f00640d89374b4f4b90d5a10f9632199e3
rachelw106 <rachelw106@nycstudents.net>
2024-10-23 13:32:53 +0000

    commit

---
+++ GridPath.java
@@ -0,0 +1,42 @@
+public class GridPath {
+    /** Initialized in the constructor with distinct values that never change */
+    private int[][] grid;
+
+    public GridPath(int[][] values)
+    {
+        grid = values;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row++)
+        {
+            for( int col = 0; col < grid[0].length; coll++)
+        }
+    }
+
+    /**
+     * Returns the Location representing a neighbor of the grid element at row and
+     * col,
+     * as described in part (a)
+     * Preconditions: row is a valid row index and col is a valid column index in
+     * grid.
+     * row and col do not specify the element in the last row and last column of
+     * grid.
+     */
+    public Location getNextLoc(int row, int col) {
+        return null; }
+
+    /**
+     * Computes and returns the sum of all values on a path through grid, as
+     * described in
+     * part (b)
+     * Preconditions: row is a valid row index and col is a valid column index in
+     * grid.
+     * row and col do not specify the element in the last row and last column of
+     * grid.
+     */
+    public int sumPath(int row, int col) {
+        return 0; }
+}
